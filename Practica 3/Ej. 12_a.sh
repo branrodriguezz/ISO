10ac720a065ea8bfe0d2472dcaed0c990a848d9c
branrodriguezz <bran3rodriguez@gmail.com>
2025-12-03 17:57:59 -0300

    Update Ej. 12_a.sh

--- Practica 3/Ej. 12_a.sh
+++ Practica 3/Ej. 12_a.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+#Script: Ejercicio12_a.sh
+#Uso: ./Ejercicio12_a.sh
 
+#Solicitud de numeros al usuario
+echo -n "Introducir primer numero: "
+read num1
+echo -n "Introducir segundo numero: "
+read num2
+
+#Algoritmo
+suma=$((num1 + num2))
+resta=$((num1 - num2))
+multiplicacion=$((num1 * num2))
+
+if [ $num1 -gt $num2 ]; then
+ mayor=$num1
+elif [ $num1 -lt $num2 ]; then
+ mayor=$num2
+else
+ mayor="Son iguales"
+fi
+
+#Resultados
+echo "----------------------"
+echo "RESULTADOS: "
+echo " - La suma es $suma"
+echo " - La resta es $resta"
+echo " - La multiplicacion es $multiplicacion"
+echo " - El mayor es : $mayor"
+echo "----------------------"
