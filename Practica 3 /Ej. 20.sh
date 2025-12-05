60e1f950dca86e2441b1bc43525c625f200b6b51
branrodriguezz <bran3rodriguez@gmail.com>
2025-12-04 23:38:30 -0300

    Create Ej. 20.sh

---
+++ Practica 3 /Ej. 20.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#Script: Ejercicio20.sh
+#Uso: ./Ejercicio20.sh
+
+#Estructura
+pila=()
+
+#Funciones
+
+#push: Recibe un parametro y lo agrega a la pila
+push(){
+ pila+=("$1")
+}
+
+#pop: Saca un elemento de la pila
+pop(){
+ if [ ${#pila[@]} -eq 0 ];then
+  echo "La pila esta vacia. No se puede hacer pop"
+ else
+  last_index=$((${#pila[@]} - 1))
+  unset "pila[$last_index]"
+ fi
+}
+
+#length: Devuelve la longitud de la pila
+length() {
+ echo "${#pila[@]}"
+}
+
+#print: Imprime todos los elementos de la pila
+print() {
+ for elem in "${pila[@]}"; do
+  echo " - $elem"
+ done
+}
+
+#Pruebas
+
+echo "Agregando 10 elementos a la pila..."
+for i in {1..10}; do
+ push "Elemento_$i"
+done
+
+echo "Sacando 3 elementos de la pila..."
+for i in {1..3}; do
+ pop
+done
+
+echo "Longitud de la pila: $(length)"
+
+echo "Impresion de la totalidad de los elementos de la pila:"
+print
