4d01c40704f9cbfa94aa26e09c90d569ecdf999d
branrodriguezz <bran3rodriguez@gmail.com>
2025-12-07 14:02:08 -0300

    Create Ej. 22.sh

---
+++ Practica 3 /Ej. 22.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Script: Ejercicio22.sh
+#Uso: ./Ejercicio22.sh
+
+#Vector
+num=(10 3 5 7 9 3 5 4)
+
+#Algoritmo
+impares=0
+for n in "${num[@]}"; do
+ if (( n % 2 == 0 )); then
+  echo "Numero par: $n"
+ else
+  ((impares++))
+ fi
+done
+
+#Resultado
+echo "Cantidad de numeros impares: $impares"
