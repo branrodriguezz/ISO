53f08e14606625eebebbdceb26b5334b6122375c
branrodriguezz <bran3rodriguez@gmail.com>
2026-06-03 09:30:12 -0300

    Create Ej. 6.sh

---
+++ Practica 3 /Ej. 6.sh
@@ -0,0 +1,10 @@
+6. El comando expr permite la evaluación de expresiones. Su sintaxis es:
+  expr arg1 op arg2,
+donde arg1 y arg2 representan argumentos y op la operación de la expresión.
+Investigar que tipo de operaciones se pueden utilizar.
+  El comando expr: (abreviatura de expresion) sirve para evaluar expresiones y mostrar su resultado por pantalla. Se usa principalmente en scripts antiguos o cuando 
+  no se dispone de la expansiom aritmetica $(()).
+  El tipo de operaciones que se pueden utilizar son:
+  - Operaciones aritmeticas: +, -, *, / (division entera), % (modulo).
+  - Operaciones relacionales o de comparacion: =, != (desigualdad de cadenas) , >, <, >=, <=. --> Evaluan cadenas.
+  - Operaciones logicas: | (or) , & (and).
