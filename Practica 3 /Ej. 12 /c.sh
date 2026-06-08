9bc18b087a655d6bf0f0a7ddea9a8780711dbf27
branrodriguezz <bran3rodriguez@gmail.com>
2026-06-08 16:18:51 -0300

    Update c.sh

--- Practica 3 /Ej. 12 /c.sh
+++ Practica 3 /Ej. 12 /c.sh
@@ -1,3 +1,6 @@
+(c) Realizar una calculadora que ejecute las 4 operaciones básicas: +, -,*, %.
+Esta calculadora debe funcionar recibiendo la operación y los números como parámetros.
+
 #!/bin/bash
 #Script: Ejercicio12_c.sh
 #Uso: ./Ejercicio12_c.sh<operacion><num1><num2>
@@ -15,7 +18,18 @@ operacion=$1
 num1=$2
 num2=$3
 
-#Validacion de numeros
+#Validacion de numeros 
+#^ (Inicio de línea): Le dice a Bash que la validación debe empezar obligatoriamente desde el primer carácter del texto.
+# No vale si hay letras al principio.
+# -? (Menos opcional): El signo - representa los números negativos, y el signo de pregunta ? significa "puede estar o no estar". 
+# Esto permite que el número sea tanto positivo (9) como negativo (-9).
+# [0-9] (Rango de dígitos): Esto es un conjunto de caracteres. 
+# En las expresiones regulares, los corchetes con un guion en medio significan "cualquier carácter individual que esté en este rango".
+# Es decir, equivale a escribir [0123456789]. Cualquier dígito del 0 al 9 es válido aquí.
+# + (Uno o más): Este símbolo aplica al bloque anterior [0-9]. Significa "tiene que haber como mínimo un dígito, pero pueden ser muchos más". 
+# Gracias a este +, el script acepta números de varias cifras como 45, 100 o 9999.
+# $ (Fin de línea): Le dice a Bash que ahí debe terminar el texto. Evita que alguien escriba algo como 95letras o 123#$.
+
 if ! [[ $num1 =~ ^-?[0-9]+$ && $num2 =~ ^-?[0-9]+$ ]]; then
  echo "Error: Los operandos deben ser numeros enteros"
  exit 1
