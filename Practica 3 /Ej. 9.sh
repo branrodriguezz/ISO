d33bf0c75a2bf0b66e9203b4f470ec888d2d5096
branrodriguezz <bran3rodriguez@gmail.com>
2026-06-08 14:46:06 -0300

    Create Ej. 9.sh

---
+++ Practica 3 /Ej. 9.sh
@@ -0,0 +1,9 @@
+9. ¿Qué acciones realizan las sentencias break y continue dentro de un bucle? ¿Qué parámetros reciben?
+  Las acciones que realizan las sentencias break y continue dentro de un bucle y los parametros que reciben son:
+    - break: el programa sale del ciclo y continua ejecutando las instrucciones que siguen despues del bucle.
+    - continue: salta a la siguiente iteracion del bucle, sin ejecutar las instrucciones restantes de la iteracion actual.
+  Ambas sentencias pueden recibir un parametro n opcional numerico, que indica cuantos niveles de bucles anidados deben afectarse.
+  - break: sale del bucle actual (por defecto).
+  - break n: sale de n niveles de bucles anidados.
+  - coninue: salta a la siguiente iteracion del bucle actual (por defecto).
+  - continue n: salta a la siguiente iteracion del n-esimo bucle exterior. 
